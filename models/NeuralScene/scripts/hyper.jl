99f77fa7aa2b893be317544715d004017efe07b1
Zenna Tavares <zennatavares@gmail.com>
2019-05-18 21:20:37 -0400

    make hyper more flexible

--- models/NeuralScene/scripts/hyper.jl
+++ models/NeuralScene/scripts/hyper.jl
@@ -2,7 +2,10 @@ using RunTools
 using NeuralScene
 using NeuralScene.Run: infer, allparams
 
-# Run
-paramsamples = rand(allparams(), 10)
-display.(paramsamples)
-control(infer, paramsamples)
+# Run from cmdline with julia -L hyper.jl -E `hyper(; params = Params(runname = pleasework))' `
+function hyper(; params = Params(), n = 10)
+  paramsamples_ = rand(allparams(), n)
+  paramsamples = [merge(params, p) for p in paramsamples_]
+  display.(paramsamples)
+  control(infer, paramsamples)
+end
