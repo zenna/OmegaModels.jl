c210b83118851abd808ebe08bdfbdaf960291f4e
Zenna Tavares <zennatavares@gmail.com>
2019-07-05 11:38:42 -0400

    bs

--- models/BlackScholes/src/BlackScholes.jl
+++ models/BlackScholes/src/BlackScholes.jl
@@ -7,7 +7,7 @@ using Lens
 using Statistics
 using Dates
 
-export bsmmc, simrv, diff_σ
+export bsmmc, simrv, diff_σ, sampleprior
 
 # TODO
 # Be able to use many observations
@@ -30,7 +30,7 @@ nsteps: number of time steps
 """
 function bsmmc(ω, σ, T = 0.5, nsteps = 16, S = 202.73, r = 0.025)  # initial stock price)
   Δt = T/nsteps
-  series = Float64[S]
+  series = Any[S]
   for i = 1:nsteps
     z = normal(ω, 0, 1)
     S = S * exp((r - 0.5σ^2) * Δt + (σ * sqrt(Δt) * z))
@@ -50,9 +50,13 @@ data = [(K = 100.0, c = 104.15, σ = .6895, T = T),
         (K = 120.0, c = 76.03, σ = .5586, T = T),
         (K = 190.0, c = 18.35, σ = .2632, T = T)]
 
-o1 = data[1]
+
+data = [(K = 100.0, c = 104.15, σ = .6895, T = T)]
+
+      
+o1 = data[1]  
 # We use priors over \sigma, which represents the volatility of the model
-const σ = uniform(0.0, 5.0)
+const σ = uniform(0.0, 1.0)
 const r = 0.025                     # risk-free
 
 # Now we create random variables for the time series, and the value of the stock at time T
@@ -60,9 +64,9 @@ const simrv = ciid(bsmmc, σ, o1.T, nsteps, AAPLS, r)
 const lastsim = lift(last)(simrv)
 
 # Let's draw some prior samples from the model
-sampleprior() = rand(simrv, 2)
-# nb plot(samplesprior())
-# savefig(fig, joinpath(FIGHOME, "bshist1.pdf"))
+sampleprior() = rand(simrv, 10)
+# nb fig =  plot(samplesprior(); xlabel = "time", ylabel = "S", legend = false)
+# savefig(fig, joinpath(FIGHOME, "bsseries1.pdf"))
 
 # Single obseration
 const diff = lift(max)(lastsim - o1.K, 0)
@@ -70,8 +74,12 @@ const diff_σ =  rid(diff, σ)
 const diffexp = samplemeanᵣ(diff_σ, 1000)
 const C_discount = diffexp * exp(-r*o1.T)
 
-run(; n = 1000, alg = SSMH, kwargs...) =
-  @leval SSMHLoop => default_cbs(n) rand(σ, diffexp ==ₛ o1.c, n; alg = alg, kwargs...)
+run(; n = 1000, alg = HMCFAST, kwargs...) =
+  @leval HMCFASTLoop => default_cbs(n) rand(σ, diffexp ==ₛ o1.c, n; alg = alg, kwargs...)
+
+runsilent(; n = 1000, alg = SSMH, kwargs...) =
+  rand(σ, diffexp ==ₛ o1.c, n; alg = alg, kwargs...)
+
 
 "Histogram of prior vs posterior samples over σ"
 function volatilityplot()
@@ -82,21 +90,29 @@ end
 # savefig(fig, joinpath(FIGHOME, "bshist1.pdf"))
 
 # Multiple observations
-function diffmulti_(ω, ks)
+function diffmulti_(ω, ks)``
   ls = lastsim(ω)
   [max(ls - k, 0) for k in ks]
 end
+
+disp(x; msg = "") = (println(msg, "value : ", x); x)
 
 const nobs = length(data)
 const ks = [o.K for o in data] 
 const cs = [o.c for o in data]
 const diffmulti = ciid(diffmulti_, ks)
 const diffmulti_σ = rid(diffmulti, σ)
-const diffmultiexp =  samplemeanᵣ(diffmulti_σ, 1000)
+const diffmultiexp_ =  samplemeanᵣ(diffmulti_σ, 100000000)
+const diffmultiexp = lift(disp)(diffmultiexp_)
 const diffmultiexpnoise = diffmultiexp + normal(0, 0.01, (nobs,))
 
-runmulti(; n = 1000, alg = Replica, kwargs...) =
+
+const condition = (samplemeanᵣ(diffmulti_σ, 10000) + normal(0, 0.01, (nobs,)))  ==ₛ cs
+runmulti(; n = 1000, alg = SSMH, kwargs...) =
   @leval SSMHLoop => default_cbs(n) rand(σ, diffmultiexpnoise ==ₛ cs, n; alg = alg, kwargs...)
+
+runmultisilent(; n = 1000, alg = Replica, kwargs...) =
+  rand(σ, diffmultiexpnoise ==ₛ cs, n; alg = alg, kwargs...)
 
 # runmulti() = @leval SSMHLoop => default_cbs(1000) rand(σ, diffmultiexpnoise ==ₛ cs, 1000; alg = Replica)
 
