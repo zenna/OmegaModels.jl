6c504dec766977ecb8b08aceaf2fb1a810c2042e
Zenna Tavares <zennatavares@gmail.com>
2019-11-14 21:40:55 -0500

    fairnes

--- models/Fairness/src/train.jl
+++ models/Fairness/src/train.jl
@@ -10,19 +10,22 @@ end
 PATH = "./"
 
 "Generate a LENS MAP"
-function genlmap()
+function genlmap(n)
   # Save the omega elements
   capture_samples = x -> Dict(:ωsamples => x.ωsamples,)
   savenet_ = incsave(joinpath(PATH, "omegas.bson"); verbose = true,
                      save = BSON.bson) ∘ capture_samples
   savenet = everyn(savenet_, 50)
+  # # @show typeof(a)
+  # @show a = [savenet default_cbs_tpl(n)...]
+  Omega.Inference.runall([savenet, default_cbs_tpl(n)...])
 end
 
 "Trian module `fairmod`, which should have `fairmod.classifier`"
-function train(fairmod; n = 100, alg = SSMH, kwargs...)
+function train(fairmod; n = 10000, alg = SSMH, kwargs...)
   classifier =~ fairmod.classifier_
   obs = Fairness.buildquery(classifier, Fairness.load_data())
-  lmap = genlmap()
+  lmap = genlmap(n)
   # default_cbs(n)
   @leval SSMHLoop => lmap rand(Ω, cond(classifier, obs), n; alg = alg, kwargs...)
 end
